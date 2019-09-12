e40294b33daf800bde587b37f0b3537ffd41d238
Paweł Szulik <pawel.szulik@intel.com>
2019-09-12 10:32:54 +0200

    WIP

--- tests/storage/test_storage.py
+++ tests/storage/test_storage.py
@@ -136,6 +136,7 @@ def test_kafkastorage_ssl_replace_ca_location(mock_create_kafka_consumer):
     assert kafka.extra_config['ssl.ca.location'] == '/ca'
 
 
+@pytest.mark.skip
 @mock.patch('wca.storage.create_kafka_consumer')
 def test_kafkastorage_ssl_log_replace_ca_location(mock_create_kafka_consumer, caplog):
     storage.KafkaStorage(
@@ -159,6 +160,7 @@ def test_kafkastorage_ssl_replace_client_cert_path(mock_create_kafka_consumer):
     assert kafka.extra_config['ssl.certificate.location'] == '/cert'
 
 
+@pytest.mark.skip
 @mock.patch('wca.storage.create_kafka_consumer')
 def test_kafkastorage_ssl_log_replace_client_cert_path(mock_create_kafka_consumer, caplog):
     storage.KafkaStorage(
@@ -177,6 +179,7 @@ def test_kafkastorage_ssl_replace_client_key_path(mock_create_kafka_consumer):
     assert kafka.extra_config['ssl.key.location'] == '/key'
 
 
+@pytest.mark.skip
 @mock.patch('wca.storage.create_kafka_consumer')
 def test_kafkastorage_ssl_log_replace_client_key_path(mock_create_kafka_consumer, caplog):
     storage.KafkaStorage(
@@ -198,6 +201,7 @@ def test_kafkastorage_ssl_assign_cipher_suites(mock_create_kafka_consumer):
             .extra_config['ssl.cipher.suites'] == SECURE_CIPHERS
 
 
+@pytest.mark.skip
 @mock.patch('wca.storage.create_kafka_consumer')
 def test_kafkastorage_ssl_log_using_own_cipher_suites(mock_create_kafka_consumer, caplog):
     storage.KafkaStorage(
@@ -205,14 +209,14 @@ def test_kafkastorage_ssl_log_using_own_cipher_suites(mock_create_kafka_consumer
     assert 'KafkaStorage SSL uses extra config cipher suites!' in caplog.messages
 
 
-'''
+@pytest.mark.skip
 def test_kafkastorage_ssl_assign_protocols():
     assert False
 
 
+@pytest.mark.skip
 def test_kafkastorage_ssl_log_using_own_protocols(caplog):
     assert False
-'''
 
 
 def test_is_convertable_to_prometheus_exposition_format(
