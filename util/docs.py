7024e7378ecd30a0b4d7f66d05390a3a44db19c5
Paweł Szulik <pawel.szulik@intel.com>
2019-11-07 13:11:53 +0100

    Add perf

--- util/docs.py
+++ util/docs.py
@@ -12,21 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from wca.metrics import METRICS_METADATA, MetricGranurality
-
-
-METRICS_DOC_PATH = 'docs/metrics.rst'
-
-INTRO = """
-================================
-Available metrics
-================================
-
-**This software is pre-production and should not be deployed to production servers.**
-
-.. contents:: Table of Contents
-
-"""
+from wca.metrics import METRICS_METADATA, MetricGranurality, MetricSource
 
 
 def prepare_csv_table(data):
@@ -43,13 +29,59 @@ def generate_title(title):
     return title + '\n' + ''.join(['=' for _ in range(len(title))])
 
 
+METRICS_DOC_PATH = 'docs/metrics.rst'
+
+INTRO = """
+================================
+Available metrics
+================================
+
+**This software is pre-production and should not be deployed to production servers.**
+
+.. contents:: Table of Contents
+
+"""
+
+PERF_BASED = generate_title("Perf event based") + """
+
+To collect metrics you need to provide `event_names` list (defaults to instructions,
+cycles, cache-misses, memstalls) to runner object in config file.
+
+**You can only collect 4 additional perf events!**
+
+"""
+
+
 def generate_docs():
 
-    task_data = []
-    platform_data = []
-    internal_data = []
+    task_data = {
+            MetricSource.PERF_EVENT: [],
+            MetricSource.CGROUP: [],
+            MetricSource.RESCTRL: [],
+            MetricSource.PROC: [],
+            MetricSource.INTERNAL: [],
+            MetricSource.GENERIC: []
+            }
 
-    for metric in METRICS_METADATA:
+    platform_data = {
+            MetricSource.PERF_EVENT: [],
+            MetricSource.CGROUP: [],
+            MetricSource.RESCTRL: [],
+            MetricSource.PROC: [],
+            MetricSource.INTERNAL: [],
+            MetricSource.GENERIC: []
+            }
+
+    internal_data = {
+            MetricSource.PERF_EVENT: [],
+            MetricSource.CGROUP: [],
+            MetricSource.RESCTRL: [],
+            MetricSource.PROC: [],
+            MetricSource.INTERNAL: [],
+            MetricSource.GENERIC: []
+            }
+
+    for metric, metadata in METRICS_METADATA.items():
         data = (metric,
                 METRICS_METADATA[metric].help,
                 METRICS_METADATA[metric].unit,
@@ -57,23 +89,25 @@ def generate_docs():
                 METRICS_METADATA[metric].source,
                 METRICS_METADATA[metric].granularity)
 
-        if data[-1] == MetricGranurality.TASK:
-            task_data.append(data)
-        elif data[-1] == MetricGranurality.PLATFORM:
-            platform_data.append(data)
-        elif data[-1] == MetricGranurality.INTERNAL:
-            internal_data.append(data)
+        if metadata.granularity == MetricGranurality.TASK:
+            task_data[metadata.source].append(data)
+        elif metadata.granularity == MetricGranurality.PLATFORM:
+            platform_data[metadata.source].append(data)
+        elif metadata.granularity == MetricGranurality.INTERNAL:
+            internal_data[metadata.source].append(data)
 
-    task_table = prepare_csv_table(task_data)
-    platform_table = prepare_csv_table(platform_data)
-    internal_table = prepare_csv_table(internal_data)
+    # task_table = prepare_csv_table(task_data)
+    # platform_table = prepare_csv_table(platform_data)
+    # internal_table = prepare_csv_table(internal_data)
 
     task_title = generate_title("Task's metrics")
     platform_title = generate_title("Platforms's metrics")
     internal_title = generate_title("Internal metrics")
 
-    docs = task_title + '\n\n' + task_table + '\n\n' + platform_title + '\n\n' + platform_table + \
-        '\n\n' + internal_title + '\n\n' + internal_table
+    #docs = task_title + '\n\n' + task_table + '\n\n' + platform_title + '\n\n' + platform_table + \
+    #    '\n\n' + internal_title + '\n\n' + internal_table
+
+    docs = task_title + '\n\n' + PERF_BASED + prepare_csv_table(task_data[MetricSource.PERF_EVENT])
 
     return docs
 
