b24c24de81613a7e314dad861419f9e191190cf1
Paweł Szulik <pawel.szulik@intel.com>
2019-12-05 13:12:11 +0100

    Allocation and unit test

--- wca/cgroups_allocations.py
+++ wca/cgroups_allocations.py
@@ -81,6 +81,7 @@ class ListFormatBasedAllocationValue(AllocationValue):
     def __init__(self, value: str, container: ContainerInterface, common_labels: dict):
         assert isinstance(value, str)
         self.cgroup = container.get_cgroup()
+        self.subcgroups = container.get_subcgroups()
         self.value = value
         self.common_labels = common_labels
         self.labels_updater = LabelsUpdater(common_labels or {})
@@ -145,7 +146,12 @@ class CPUSetCPUSAllocationValue(ListFormatBasedAllocationValue):
 
     def perform_allocations(self):
         cpus = decode_listformat(self.value)
-        self.cgroup.set_cpuset_cpus(cpus)
+
+        if len(self.subcgroups) > 0:
+            for subcgroup in self.subcgroups:
+                subcgroup.set_cpuset_cpus(cpus)
+        else:
+            self.cgroup.set_cpuset_cpus(cpus)
 
 
 class CPUSetMEMSAllocationValue(ListFormatBasedAllocationValue):
